a6125426da02b52c04c800ab09232ca3c5bbc01d
17ColinMiPerry <17colinmiperry@gmail.com>
2025-04-16 12:26:31 -0700

    updated aboutme text

--- src/pages/Home/appearance/AboutMeText/index.jsx
+++ src/pages/Home/appearance/AboutMeText/index.jsx
@@ -4,7 +4,8 @@ function AboutMeText() {
           <h1 className='text-4xl md:text-5xl font-bold pb-6 text-center md:text-left w-full'>
               <span className='text-blue-300'>Hey!</span> I'm Colin Perry
           </h1>
-          <p className='text-gray-400 text-lg font-semibold text-center md:text-left'>I'm a software engineer with a passion for building products that help people live better lives. heres a lil more text to see how it looks
+          <p className='text-gray-400 text-lg font-semibold text-center md:text-left'>
+            I want to build products that help people live better lives, and learn as much as I can along the way.
           </p>
       </div>
   )
