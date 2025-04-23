42ee8a8071f4b5108e4e805a21bfd80aaea396a4
17ColinMiPerry <17colinmiperry@gmail.com>
2025-04-23 10:42:43 -0700

    improved external link logic

--- src/pages/Contact/appearance/SocialMediaIcons/index.jsx
+++ src/pages/Contact/appearance/SocialMediaIcons/index.jsx
@@ -1,32 +1,31 @@
 import { SiGithub, SiLinkedin, SiGmail } from "react-icons/si";
-import { Link } from 'react-router-dom';
 
 function SocialMediaIcons() {
     return (
         <div className='flex flex-col gap-4 font-semibold'>
             <div className='flex flex-row gap-4 items-center'>
-                <Link to="https://www.linkedin.com/in/colin-perry-499304180/">
+                <a href="https://www.linkedin.com/in/colin-perry-499304180/" target="_blank" rel="noopener noreferrer">
                     <SiLinkedin className='w-8 h-8 opacity-100 hover:shadow-sm hover:opacity-60 transition duration-300' />
-                </Link>
-                <Link to="https://www.linkedin.com/in/colin-perry-499304180/">
+                </a>
+                <a href="https://www.linkedin.com/in/colin-perry-499304180/" target="_blank" rel="noopener noreferrer">
                     <h1 className='text-purple-400 hover:text-purple-500 transition duration-300'>Colin Perry</h1>
-                </Link>
+                </a>
             </div>
             <div className='flex flex-row gap-4 items-center'>
-                <Link to="https://github.com/17ColinMiPerry">
+                <a href="https://github.com/17ColinMiPerry" target="_blank" rel="noopener noreferrer">
                     <SiGithub className='w-8 h-8 opacity-100 hover:shadow-sm hover:opacity-60 transition duration-300' />
-                </Link>
-                <Link to="https://github.com/17ColinMiPerry">
+                </a>
+                <a href="https://github.com/17ColinMiPerry" target="_blank" rel="noopener noreferrer">
                     <h1 className='text-purple-400 hover:text-purple-500 transition duration-300'>17ColinMiPerry</h1>
-                </Link>
+                </a>
             </div>
             <div className='flex flex-row gap-4 items-center'>
-                <Link to="mailto:17colinmiperry@gmail.com">
+                <a href="mailto:17colinmiperry@gmail.com">
                     <SiGmail className='w-8 h-8 opacity-100 hover:shadow-sm hover:opacity-60 transition duration-300' />
-                </Link>
-                <Link to="mailto:17colinmiperry@gmail.com">
+                </a>
+                <a href="mailto:17colinmiperry@gmail.com">
                     <h1 className='text-purple-400 hover:text-purple-500 transition duration-300'>17colinmiperry@gmail.com</h1>
-                </Link>
+                </a>
             </div>
         </div>
     )
--- src/pages/Home/appearance/SocialMediaIcons/index.jsx
+++ src/pages/Home/appearance/SocialMediaIcons/index.jsx
@@ -1,18 +1,17 @@
 import { SiGithub, SiLinkedin, SiGmail } from "react-icons/si";
-import { Link } from 'react-router-dom';
 
 function SocialMediaIcons() {
     return (
         <div className='flex flex-row gap-4'>
-            <Link to="https://www.linkedin.com/in/colin-perry-499304180/">
+            <a href="https://www.linkedin.com/in/colin-perry-499304180/" target="_blank" rel="noopener noreferrer">
                 <SiLinkedin className='w-8 h-8 opacity-100 hover:opacity-60 transition duration-300' />
-            </Link>
-            <Link to="https://github.com/17ColinMiPerry">
+            </a>
+            <a href="https://github.com/17ColinMiPerry" target="_blank" rel="noopener noreferrer">
                 <SiGithub className='w-8 h-8 opacity-100 hover:opacity-60 transition duration-300' />
-            </Link>
-            <Link to="mailto:17colinmiperry@gmail.com">
+            </a>
+            <a href="mailto:17colinmiperry@gmail.com">
                 <SiGmail className='w-8 h-8 opacity-100 hover:opacity-60 transition duration-300' />
-            </Link>
+            </a>
         </div>
     )
 }
