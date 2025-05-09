d8d26366ed93baf9e6dfa7edf9cdf7858dff362e
17ColinMiPerry <17colinmiperry@gmail.com>
2025-05-08 17:17:07 -0700

    updated project blurb

--- src/pages/Home/appearance/ProjectBlurb/index.jsx
+++ src/pages/Home/appearance/ProjectBlurb/index.jsx
@@ -11,7 +11,7 @@ function ProjectBlurb() {
         >
             <div className="flex flex-col w-full justify-center items-center p-6">
                 <h1 className="text-4xl font-bold">Projects</h1>
-                <p className="text-gray-400 text-lg font-semibold pt-4">Here are some things I'm proud of - </p>
+                <p className="text-gray-400 text-lg font-semibold pt-4">Here are some recent projects that I'm proud of - </p>
                 <p className="text-gray-400 text-lg font-semibold">Click on a project to learn more!</p>
             </div>
             <Projects />
