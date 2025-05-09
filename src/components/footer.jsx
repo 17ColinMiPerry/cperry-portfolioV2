6c9cef21cdc975c94d7050f490c28ecb5b936566
17ColinMiPerry <17colinmiperry@gmail.com>
2025-05-08 17:18:03 -0700

    made footer transparent

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <footer className='w-full bg-gray-900/35 text-white flex items-center justify-end py-2 px-4 mt-16'>
+        <footer className='w-full bg-transparent text-white flex items-center justify-end py-2 px-4 mt-16'>
             <p className='text-md'>Any issues? Send an email to <a href="mailto:Cperry.webmaster@gmail.com" className='underline font-semibold text-purple-400 hover:text-purple-500 transition duration-300'>Cperry.webmaster@gmail.com</a> to let me know!</p>
         </footer>
     )
