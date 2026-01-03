f9d026e91e3d83c9a1522293249228be9320342e
Colin Perry <17ColinMiPerry@gmail.com>
2026-01-03 15:30:51 -0800

    updated footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
     return (
         <footer className='w-full bg-transparent text-white flex items-center justify-end py-2 px-4 mt-16'>
-            <p className='text-md'>Any issues? Send an email to <a href="mailto:Cperry.webmaster@gmail.com" className='underline font-semibold text-purple-400 hover:text-purple-500 transition duration-300'>Cperry.webmaster@gmail.com</a> to let me know!</p>
+            <p className='text-md'>Any issues? Send an email to <a href="mailto:cperry.webmaster@gmail.com" className='underline font-semibold text-purple-400 hover:text-purple-500 transition duration-300'>Cperry.webmaster@gmail.com</a> to let me know!</p>
         </footer>
     )
 }
