1da6f4282c3c99dc1c79434606100a7d284d25c7
Colin Perry <17ColinMiPerry@gmail.com>
2026-01-06 10:45:57 -0800

    updated about me section

--- src/pages/Home/appearance/AboutMeText/index.jsx
+++ src/pages/Home/appearance/AboutMeText/index.jsx
@@ -5,7 +5,7 @@ function AboutMeText() {
                 <span className='text-blue-300'>Hey!</span> I'm <span className='text-gradient'>Colin Perry</span>
             </h1>
             <p className='text-gray-400 text-lg font-medium text-center md:text-left leading-relaxed'>
-                I am a fullstack developer building apps with React, Node.js, Golang, and modern cloud tools. I love challenging problems, so please reach out if you'd like to work together!
+                I am a fullstack developer building apps with React, Node.js, Golang, and modern cloud tools. I love taking ideas from <span className="inline-flex whitespace-nowrap align-baseline"> 0 ▹ 1, </ span> so please reach out if you'd like to work together!
             </p>
         </div>
     )
