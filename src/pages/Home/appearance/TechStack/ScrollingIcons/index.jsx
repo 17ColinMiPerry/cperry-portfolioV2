45c283fb014c822e6032f3f0cc063ecfdbf52e16
Colin Perry <17ColinMiPerry@gmail.com>
2026-01-07 13:39:50 -0800

    added tech

--- src/pages/Home/appearance/TechStack/ScrollingIcons/index.jsx
+++ src/pages/Home/appearance/TechStack/ScrollingIcons/index.jsx
@@ -7,6 +7,8 @@ import solidity from '../assets/solidity-logo.png';
 import github from '../assets/github.svg';
 import react from '../assets/react.svg';
 import vite from '../assets/vite.svg';
+import go from '../assets/go.png'
+import ts from '../assets/ts.png'
 
 const techIcons = [
     { name: 'C', logo: c },
@@ -16,34 +18,36 @@ const techIcons = [
     { name: 'GitHub', logo: github },
     { name: 'React', logo: react },
     { name: 'Vite', logo: vite },
+    { name: 'TypeScript', logo: ts },
+    { name: 'Go', logo: go }
 ]
 
 function ScrollingIcons() {
     return (
         <div className="overflow-hidden w-full mx-auto p-4">
-            <Marquee 
-                loop={0} 
-                speed={50} 
+            <Marquee
+                loop={0}
+                speed={50}
                 gradient={false}
                 delay={0}
                 direction="left"
                 className="py-6"
             >
                 {techIcons.map((icon, index) => (
-                    <img 
+                    <img
                         key={`${icon.name}-${index}`}
-                        src={icon.logo} 
-                        alt={icon.name} 
-                        className='h-16 md:h-24 inline-block mx-6 md:mx-12 hover:drop-shadow-[0_0_20px_rgba(59,130,246,0.9)]' 
+                        src={icon.logo}
+                        alt={icon.name}
+                        className='h-16 md:h-24 inline-block mx-6 md:mx-12 hover:drop-shadow-[0_0_20px_rgba(59,130,246,0.9)]'
                     />
                 ))}
-                
+
                 {techIcons.map((icon, index) => (
-                    <img 
+                    <img
                         key={`${icon.name}-duplicate-${index}`}
-                        src={icon.logo} 
-                        alt={icon.name} 
-                        className='h-16 md:h-24 inline-block mx-6 md:mx-12 hover:drop-shadow-[0_0_20px_rgba(59,130,246,0.9)]' 
+                        src={icon.logo}
+                        alt={icon.name}
+                        className='h-16 md:h-24 inline-block mx-6 md:mx-12 hover:drop-shadow-[0_0_20px_rgba(59,130,246,0.9)]'
                     />
                 ))}
             </Marquee>
